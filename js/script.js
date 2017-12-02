0039cf55eaa7dce12a56491524eceb78a64bc9a2
dmsobel <david95@sobel.net>
2017-12-01 22:54:32 -0600

    fixed validation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function semsel(value) {
     prompt("Enter your alternate pin: ");
     $('.classes').show();
 }
+
+function navbarScale() {
+    var topbar = document.getElementById("topnavbar");
+    if (topbar.className === "topnav") {
+        topbar.className += " responsive";
+    } else {
+        topbar.className = "topnav";
+    }
+}
